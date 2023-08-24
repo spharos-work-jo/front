eb219380a60b1302b87368043e4a1b9ec4ed9964
Jason Ahn <42033861+beat1103@users.noreply.github.com>
2023-08-24 17:24:18 +0900

    fixed: tailwind css convert

--- ssg-point/components/pages/signup/signupInfoBottom.tsx
+++ ssg-point/components/pages/signup/signupInfoBottom.tsx
@@ -1,33 +1,41 @@
 import React from "react";
-import styles from "./signupInfoBottom.module.css";
+import { signUpListData } from "@/data/signUpListData";
+import Image from "next/image";
+import Link from "next/link";
+// import styles from "./signupInfoBottom.module.css";
 
 const signupInfoBottom = () => {
     return(
         <>
         <div className="px-5 pb-8">
-                <ol className="relative pb-90 h-92">
-                  <li className="relative box-border break-after-auto">
-                    <span className="">STEP.1본인인증</span>
+            <ol>
+              {
+                signUpListData.map( item =>(
+                  <li className="flex justify-start items-center gap-2 border-b border-black-50 p-4 last:border-none" key={item.id}>
+                    <Image 
+                      src={item.icon}
+                      alt={item.title}
+                      width={60}
+                      height={62}
+                      className="pr-4"
+                    />
+                    <span className="text-xs">{item.step}</span>
+                    <span className="text-base">{item.title}</span>
                   </li>
-                  <li className={styles.step_list_li}>
-                    <span className={styles.txt0}>STEP.2</span>
-                    <span className={styles.txt1}>약관동의</span>
-                  </li>
-                  <li className={styles.step3}>
-                    <span className={styles.txt0}>STEP.3</span>
-                    <span className={styles.txt1}>정보입력</span>
-                  </li>
-                  <li className={styles.step4}>
-                    <span className={styles.txt0}>STEP.4</span>
-                    <span className={styles.txt1}>가입완료</span>
-                  </li>
-                </ol>
-                <div className={styles.btn_box}>
-                  <a href='signup/cert' className={styles.btn_primary}>회원가입</a>
-                  <p className={styles.sub_txt0}>만 14세 미만 아동은 회원가입할 수 없습니다.</p>
-                </div>
-              </div>
-              </>
+                ))
+              }
+            </ol>
+            <div className='w-full'>
+    
+              <Link href='signup/cert'>
+                <p className='w-full p-4 text-center text-black text-sm rounded-md'>
+                  회원가입
+                </p>
+              </Link>
+              <p className=''>만 14세 미만 아동은 회원가입할 수 없습니다.</p>
+            </div>
+          </div>
+        </>
     );
 };
 export default signupInfoBottom;
