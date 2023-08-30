fb6f1115a351eedc5452c2ecccaeb271f804975b
HJ <onmer99@gmail.com>
2023-08-30 11:05:52 +0900

    feat: 로그인 Auth 연동

--- ssg-point/app/coupon/page.tsx
+++ ssg-point/app/coupon/page.tsx
@@ -2,8 +2,22 @@ import React from 'react'
 import CouponPageTop from '@/components/pages/coupon-page/CouponPageTop';
 import CouponPageBot from '@/components/pages/coupon-page/CouponPageBot';
 import CouponList from '@/components/pages/coupon-page/CouponList';
+import { getServerSession } from "next-auth/next"
 
-const coupon = () => { 
+// import { options } from "../api/auth/[...nextauth]/options"
+
+import { redirect } from "next/navigation";
+import { signIn } from "next-auth/react";
+
+export default async function coupon() { 
+
+
+  // const session = await getServerSession(options)
+
+  // if (!session) {
+  //    redirect('/api/auth/signin?callbackUrl=/coupon')
+  // }
+
   return (
     <section className="pt-[4rem]">
 
@@ -14,5 +28,3 @@ const coupon = () => {
     </section>
   )
 }
-
-export default coupon;
--- ssg-point/components/ui/MenuBar.tsx
+++ ssg-point/components/ui/MenuBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 const bar = {
   position: "absolute",
   top: '0',
@@ -14,17 +15,31 @@ const bar = {
   borderBottom: "1px solid #e8e8e8",
 };
 
-
 // className='menu_bar top-[56px] opacity-100 absolute left-0 w-full bg-white h-[48px] overflow-hidden [z-index:1]
 
-export const menubar = () => {
-  return (
-    <div>
-      <ul className=''>
-      <li>
+const MenuBar = (props: {
+  menuList: MenubarType[];
+  selected: string;
+  setSelected: React.Dispatch<React.SetStateAction<string>>;
 
-      </li>
-      </ul>
+}) => {
+  
+  
+  const { menuList, selected, setSelected } = props;
+
+  return (
+    <div className={style.bar}> 
+    <ul className='flex w-full h-full box-border [justify-content:left] flex-nowrap overflow-x-auto '>
+    <li className='pl-[20px] pr-[20px] flex-auto h-full box-border relative'>
+    <section className=''>
+      <BarWrap
+        barlist={barList}
+
+        />
+    </section>
+    </li>
+    </ul>
     </div>
   )
 }
+export const MenuBar;
