efd0c8ef39e15deca66975ceb129d51a9b9cf09c
DESKTOP-KA9I336\교육생42 <affogato128@gmail.com>
2023-08-31 21:37:49 +0900

    address api & input profile 50%

--- ssg-point/app/(auth)/signup/profile-input/page.tsx
+++ ssg-point/app/(auth)/signup/profile-input/page.tsx
@@ -1,5 +1,4 @@
 import ProfileInputBody from "@/components/pages/profile-input/ProfileInputBody";
-import ProfileInputBottom from "@/components/pages/profile-input/ProfileInputBottom";
 import ProfileInputTop from "@/components/pages/profile-input/ProfileInputTop";
 import React from "react";
 
@@ -9,7 +8,6 @@ function Page() {
       <section className="pt-20 pb-10 block">
         <ProfileInputTop/>
         <ProfileInputBody/>        
-        <ProfileInputBottom/>
       </section>
     </>
   );
