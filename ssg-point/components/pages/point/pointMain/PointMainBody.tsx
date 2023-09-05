28f76839eb6fdebe7b8613586b9110eb794fff35
DESKTOP-KA9I336\교육생42 <affogato128@gmail.com>
2023-09-05 18:29:12 +0900

    Point Main Page 50%

--- ssg-point/components/pages/point/pointMain/PointMainBody.tsx
+++ ssg-point/components/pages/point/pointMain/PointMainBody.tsx
@@ -1,10 +1,25 @@
+'use client'
+
 import { pointHistoryMenuList, pointHistoryMenuListType } from "@/data/pointHistoryMenuList";
 import { timeFilter } from "@/data/timeFilter";
 import Image from "next/image";
-import React from "react";
+import React, { useState} from "react";
 
 function PointMainBody() {
 
+  const [savePoint,setSavePoint] = useState<number>(0);
+  const [usedPoint,setUsedPoint] = useState<number>(0);
+  const [emptyPointHistory,setEmptyPointHistory] = useState<boolean>(false);
+  const [selectPointHistory,setSelectPointHistory] = useState<number>(0);
+
+  const handleOnClick = (e: any) => {
+
+    const {id} = e.target.id;
+
+    setSelectPointHistory(id);
+    // fetch보낼때 selectPointHistory (1 ~ 4)에 따라 API호출
+    // + 1주일 ~ 6개월 시간 정보까지 같이 보냄
+  }
   return (
     <>
       <div>
@@ -14,6 +29,7 @@ function PointMainBody() {
             <li 
               key={item.id}
               className="text-sm"
+              onClick={handleOnClick}
             >
               {item.name}
             </li>
@@ -34,13 +50,36 @@ function PointMainBody() {
               ))
             }
             </select>
-            <p className="w-full block bg-gray-100 h-8 mt-2 flex">
-              <Image
-                alt="포인트 플러스 아이콘"
-                src=""
-                />
-            </p>
           </div>
+          
+            <div className="w-full mt-2 pt-1 h-6 bg-gray-100">
+              <div className="mx-2 h-4 flex">
+                <Image
+                  alt="포인트 플러스 아이콘"
+                  src="/assets/images/point/pointPlusIcon.png"
+                  height={2}
+                  width={16}
+                  />
+                    <p className="mx-3 text-[#EA035C] text-xs">적립 {savePoint}P</p>
+                <Image
+                  alt="포인트 마이너스 아이콘"
+                  src="/assets/images/point/pointMinusIcon.png"
+                  height={2}
+                  width={16}
+                  />
+                <p className="mx-2 text-xs">사용 {usedPoint}P</p>
+            </div>
+          </div>
+          <div className="flex justify-center items-center py-32">
+            { emptyPointHistory ? null :
+              <Image
+                alt="빈 내역창 아이콘"
+                height={48}
+                width={48}
+                src="/assets/images/point/noPointIcon.png"
+              />
+            }
+        </div>
       </div>
     </>
   );
